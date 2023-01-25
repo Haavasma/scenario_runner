e4da04564a87cd59effd66dd2d8ca81b47476a6a
haavasma <haavasma@stud.ntnu.no>
2023-01-25 12:28:29 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scenario_runner",
+    version="0.9.13",
+    description="Scenario Runner",
+    packages=[package for package in find_packages() if package.startswith("srunner")],
+    py_modules=["scenario_runner", "srunner"],
+    install_requires=[
+        "py-trees==0.8.3",
+        "networkx==2.2",
+        "Shapely==1.7.1",
+        "psutil",
+        "xmlschema==1.0.18",
+        "ephem",
+        "tabulate",
+        "opencv-python==4.2.0.32",
+        "numpy==1.18.1",
+        "matplotlib",
+        "six",
+        "simple-watchdog-timer",
+    ],
+)
