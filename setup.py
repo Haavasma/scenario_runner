a396ac947f2b44b602e05e1749b482848f2cb39a
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-02-08 14:22:49 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ setup(
     name="scenario_runner",
     version="0.9.13",
     description="Scenario Runner",
-    packages=[
-        package
-        for package in find_packages()
-        if package.startswith("srunner") or package.startswith("agents")
-    ],
-    py_modules=["scenario_runner", "srunner", "manual_control", "agents"],
+    # include srunner and agents modules
+    packages=find_packages(),
+    # include scenario_runner.py
+    py_modules=["scenario_runner, agents"],
     install_requires=[
         "py-trees==0.8.3",
         "networkx==2.2",
