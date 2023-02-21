4c784621698cd14fce2987d4d79d1dd7067fb162
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-02-21 06:15:23 +0100

    setup script for scenario_runner for leaderboard 2.0

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+def print_package(package):
+    print(package)
+    return package
+
+
+setup(
+    name="scenario_runner",
+    version="0.9.13",
+    description="Scenario Runner",
+    # include srunner and agents modules
+    packages=[
+        package
+        for package in find_packages()
+        if package.startswith("srunner") or package.startswith("agents")
+    ],
+    # include scenario_runner.py
+    py_modules=["scenario_runner", "agents", "srunner", "manual_control"],
+    install_requires=[
+        "py-trees==0.8.3",
+        "networkx==2.2",
+        "Shapely==1.7.1",
+        "psutil",
+        "xmlschema==1.0.18",
+        "ephem",
+        "tabulate",
+        "opencv-python==4.2.0.32",
+        "numpy>=1.21.0, <1.24.0",
+        "matplotlib",
+        "six",
+        "simple-watchdog-timer",
+        "carla==0.9.13",
+    ],
+)
